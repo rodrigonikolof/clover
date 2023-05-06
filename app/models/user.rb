574524900a34c3783415bf8dc9a842661759beaf
Rodrigo Nikolof <rodrigonikolof@outlook.com>
2023-05-06 13:03:58 +1000

    Added validations to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
     validates :name, :email, :password, presence: true
+    validates :email, uniqueness: true
+    validates :password, length: {minimum: 6}
 end
