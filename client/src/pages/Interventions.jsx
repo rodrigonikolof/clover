28c55c95a05cf57d96a0679fb9c5959460d16ce5
Rodrigo Nikolof <rodrigonikolof@outlook.com>
2023-05-14 16:09:20 +1000

    Created function to fetch interventions

--- client/src/pages/Interventions.jsx
+++ client/src/pages/Interventions.jsx
@@ -1,14 +1,35 @@
 import React, {useState, useEffect, useContext} from "react";
 import { Context } from "../App";
+import { Box, Typography } from "@mui/material";
 
 export default function Interventions(){
     const [user, setUser, token, setToken] = useContext(Context);
+    const [interventions, setInterventions] = useState(null)
 
-
+    useEffect(()=>{
+        fetch('/api/v1/interventions',{
+            method: 'GET',
+            headers: {
+                Authorization : `Bearer ${token}`
+            }
+        }).then(r => r.json()).then(data => setInterventions(data))
+    },[])
+    console.log(interventions)
 
 return(
     <>
-        <h1>Interventions</h1>
+        <Box
+            sx={{display: 'flex', justifyContent: 'center', mt: 3}}
+            >
+                <Typography
+                variant="h5" 
+                component="h2" 
+                color="textSecondary"
+                gutterBottom
+                >
+                    Interventions
+                </Typography>
+            </Box>
     </>
 )
 
