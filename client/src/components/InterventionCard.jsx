c72bddc740be341c23198303f61e9b0c66df093f
Rodrigo Nikolof <rodrigonikolof@outlook.com>
2023-05-15 15:16:30 +1000

    Added minor validations and toggle switch to update intervention

--- client/src/components/InterventionCard.jsx
+++ client/src/components/InterventionCard.jsx
@@ -1,22 +1,60 @@
-import React from "react";
-import { Box, Typography, Button, Grid, Paper, Tooltip, IconButton } from "@mui/material";
+import React, {useState, useEffect} from "react";
+import { Box, Typography, Button, Grid, Paper, Tooltip, IconButton, TextField } from "@mui/material";
 import EditNoteIcon from '@mui/icons-material/EditNote';
 import DeleteIcon from '@mui/icons-material/Delete';
 
 
 export default function InterventionCard({intervention}){
+    const [showUpdate, setShowUpdate] = useState(false);
+    const [interventionName, setInterventionName] = useState('');
+
+    useEffect(()=>{
+        setInterventionName(intervention.intervention_name)
+    },[])
     
+    const toggleShowUpdate = ()=>{
+        setShowUpdate(!showUpdate)
+    }
+    
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+        if (interventionName === intervention.intervention_name || interventionName === "")
+            toggleShowUpdate()
+        else
+            console.log('it worked')
+
+    }
+
+    
+
     return(
     <>
+    
         <Paper sx={{textAlign: 'center', lineHeight: '10px'}}>
             <Box sx={{display: 'flex'}}>
                 <Box sx={{flexGrow: 1}}>
-                    <Typography sx={{mt:1}}>{intervention.intervention_name}</Typography>
+                {showUpdate ? 
+                    <form onSubmit={handleSubmit}>
+                        <TextField
+                            onChange={(e)=>{setInterventionName(e.target.value)}}
+                            defaultValue={interventionName}
+                            sx={{maxHeight:-1}}
+                        />
+                        <Button
+                        type="submit"
+                        color="primary"
+                        variant="contained"
+                        >
+                            Save
+                        </Button>
+                    </form> 
+                    : 
+                    <Typography sx={{mt:1}}>{interventionName}</Typography>}
                 </Box>
                 <Box>
                     <Tooltip title="Edit">
-                        <IconButton>
-                            <EditNoteIcon sx={{mr:1}}/>
+                        <IconButton onClick={toggleShowUpdate}>
+                            <EditNoteIcon sx={{mr:1}} />
                         </IconButton>
                     </Tooltip>
                     <Tooltip title="Delete">
@@ -25,9 +63,13 @@ export default function InterventionCard({intervention}){
                         </IconButton>
                     </Tooltip>
                 </Box>
-            </Box>
+            </Box> 
+            
+            
             
         </Paper>
+        
+
     </>
     )
 }
