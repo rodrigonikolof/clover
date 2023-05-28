a6952d132f5a62104ce29649708c546c28927353
Rodrigo Nikolof <rodrigonikolof@outlook.com>
2023-05-28 14:40:24 +1000

    Added Button to create new goal in Goals component

--- client/src/components/Goals.jsx
+++ client/src/components/Goals.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState, useContext } from "react";
 import { Context } from "../App";
 import { Button, Typography, Box } from "@mui/material";
 import SingleGoal from "./SingleGoal";
+import AddIcon from '@mui/icons-material/Add';
 
 export default function Goals({client_id}){
 
@@ -18,8 +19,20 @@ export default function Goals({client_id}){
 console.log(goals)
 
     return(
-        <>
-            <Box sx={{display:'block', minWidth:1}}>
+        <Box sx={{width: 1, mt:5}}>
+            <Box sx={{display: 'flex', minWidth: 1}}>
+                <Box sx={{flexGrow: 1, justifyContent:'center', display:'flex'}}><Typography variant="h5" color='textSecondary'>Goals</Typography></Box>
+                <Box>
+                    <Button
+                        color="primary"
+                        variant="contained"
+                        endIcon={<AddIcon/>}
+                    >
+                    New Goal
+                    </Button>
+                </Box>
+            </Box>
+            <Box sx={{display:'block', minWidth:1, mt:2}}>
                 {goals? 
                     goals.map((goal)=>{
                     return   (
@@ -30,6 +43,6 @@ console.log(goals)
                     null
                 }
             </Box>
-        </>
+        </Box>
     )
 }
