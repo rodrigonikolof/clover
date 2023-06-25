80234c664de535a36dddb7952ae11b3d48c0d3f8
Rodrigo Nikolof <rodrigonikolof@outlook.com>
2023-06-25 15:25:52 +1000

    Added columns to users table to hold reset password token and time sent

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_02_034123) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_25_052243) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,6 +51,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_02_034123) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
   end
 
 end
